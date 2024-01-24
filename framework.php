8ce6e5beb9cf1be620fb28b39defa43251f39074
rajan <rajan.okita@strathmore.edu>
2024-01-24 09:29:25 +0300

    Basic format

---
+++ framework.php
@@ -0,0 +1,20 @@
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+
+    <a class="navbar-brand" href="#">
+        <img src="logo.jpeg" alt="logo" style="width:60px;">
+    </a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Book a room</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+        </li>
+    </ul>
+</nav>
+

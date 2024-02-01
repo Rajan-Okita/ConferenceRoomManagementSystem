e35e2666b7d7f8c8070d2b88a4a1ded0acbba6fe
rajan <rajan.okita@strathmore.edu>
2024-02-01 09:56:48 +0300

    Slight change in echo message

--- auth/config/connection.php
+++ auth/config/connection.php
@@ -7,6 +7,6 @@ $database="crown";
 $con = mysqli_connect($servername,$username,$password,$database);
 
 if ($con) {
-   echo "Successful connection" . '<br>';
+  // echo "Successful connection" . '<br>';
 } else
     die('connection failed' . mysqli_connect_errno());

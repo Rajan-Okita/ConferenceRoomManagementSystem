cb957b13023601827b424d3919fe958e969d1874
rajan <rajan.okita@strathmore.edu>
2024-02-01 09:57:41 +0300

    Checking if email exists

--- auth/config/process_register.php
+++ auth/config/process_register.php
@@ -1,19 +1,26 @@
 <?php
 require('connection.php');
 
-$f_name=$_POST['first_name'];
-$l_name=$_POST['last_name'];
-$email=$_POST['email'];
-$password=password_hash($_POST['password'],PASSWORD_DEFAULT);
-$role=$_POST['role'];
+    $f_name = $_POST['first_name'];
+    $l_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
 
-$qry=("insert into users_tbl(first_name,last_name,email_address,password,role)
+$email_check_query="select * from users_tbl where email_address='$email'" ;
+$result=mysqli_query($con,$email_check_query);
+if(mysqli_num_rows($result)>0){
+    echo "email already exists";
+}else {
+
+    $qry = ("insert into users_tbl(first_name,last_name,email_address,password,role)
 values('$f_name','$l_name','$email','$password','$role')");
 
-$qryCon= mysqli_query($con,$qry);
+    $qryCon = mysqli_query($con, $qry);
 
-if ($qryCon) {
-    header("location:../../index.php");
-   }else{
-    echo 'error' .mysqli_error($con);
+    if ($qryCon) {
+        header("location:../../index.php");
+    } else {
+        echo 'error' . mysqli_error($con);
+    }
 }

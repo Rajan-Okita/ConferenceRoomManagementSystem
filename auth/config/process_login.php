9ad1f4fd49da1f17e7b67fec8027765674484b05
rajan <rajan.okita@strathmore.edu>
2024-02-05 14:42:26 +0300

    Login

--- auth/config/process_login.php
+++ auth/config/process_login.php
@@ -1,29 +1,43 @@
 <?php
 require("connection.php");
 session_start();
-if(isset($_POST['password']) && isset($_POST['email'])) {
-    $password = $_POST['password'];
-    $email = mysqli_real_escape_string($con, $_POST['email']);
 
-    if (!empty($email) && !empty($password)) {
-        $query = "select * from users_tbl where email_address='$email' and password='$password'";
-        $result = mysqli_query($con, $query);
-        if (mysqli_num_rows($result) == 1) {
-            $row = mysqli_fetch_assoc($result);
-            $_SESSION['sessionuser'] = $row['first_name'];
-            $_SESSION['sessionname'] = $row['last_name'];
-            if ($row['role'] == '1') {
-                $_SESSION['sessionid']= $row['role'];
-                header("location:../../homepage.php");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['password']) && isset($_POST['email'])) {
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+
+        if (!empty($email) && !empty($password)) {
+
+            $query = "SELECT * FROM users_tbl WHERE email_address='$email'";
+            $result = mysqli_query($con, $query);
+
+            if ($result && mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+
+                if (password_verify($password, $row['password'])) {
+                    $_SESSION['sessionuser'] = $row['first_name'];
+                    $_SESSION['sessionname'] = $row['last_name'];
+                    if ($row['role'] == 'user') {
+                        $_SESSION['sessionid'] = $row['role'];
+                        header("location:../../homepage.php");
+                        exit();
+                    } else {
+                        header("location:../../adminpage.php");
+                        exit();
+                    }
+                } else {
+
+                    echo "Invalid password";
+                }
             } else {
-                header("location:../../adminpage.php");
+
+                echo "User not found";
             }
-            exit();
         } else {
-            exit();
+
+            echo "Email and password are required";
         }
-    } else {
-        echo "Invalid credentials";
     }
 }
 ?>

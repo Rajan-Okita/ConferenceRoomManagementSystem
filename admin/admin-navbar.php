b484cd9119d521650165f2e73290ed6ed0b66bbe
rajan <rajan.okita@strathmore.edu>
2024-03-04 11:50:40 +0300

    Admin dashboard

---
+++ admin/admin-navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+
+    <a class="navbar-brand" href="#">
+        <img src="../logo.jpeg" alt="logo" style="width:60px;">
+    </a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="admin_page.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+        </li>
+    </ul>
+</nav>

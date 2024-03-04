55ff88b2a9b4bd806b6fc0b1d2d7e38faae996af
rajan <rajan.okita@strathmore.edu>
2024-03-04 11:49:26 +0300

    Admin dashboard

--- admin/view_rooms.php
+++ admin/view_rooms.php
@@ -1,3 +1,5 @@
+<?php require('admin-navbar.php');?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,23 +14,6 @@
 
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-
-    <a class="navbar-brand" href="#">
-        <img src="../logo.jpeg" alt="logo" style="width:60px;">
-    </a>
-
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="admin_page.php">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Help</a>
-        </li>
-    </ul>
-</nav>
-
-
 <div class="container mt-5">
     <?php
     session_start();
